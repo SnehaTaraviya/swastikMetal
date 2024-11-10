8edf8d8165dc2216df3b80ed3491240f56572977
Sneha Taraviya <info.sneha12401@gmail.com>
2024-11-10 17:48:06 +0530

    update model

--- swastikProject/swastik/models.py
+++ swastikProject/swastik/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Getintouch(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=12)
+    company = models.CharField(max_length=30)
+    address = models.CharField(max_length=1000)
+    message = models.TextField(max_length=5000)
+    
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
--- swastikProject/swastik/views.py
+++ swastikProject/swastik/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import Getintouch
 # Create your views here.
 
 def home(request):
@@ -12,6 +14,20 @@ def aboutus(request):
     return render(request, 'aboutus.html')
 
 def getintouch(request):
+   
+    if request.method == "POST":
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        company = request.POST.get('company')
+        address = request.POST.get('address')
+        message = request.POST.get('message')
+        getintouchs = Getintouch(firstname = firstname, lastname = lastname,email = email, phone = phone, company = company,address = address,message = message)
+        getintouchs.save()
+        sendMailToHost(firstname,lastname,email,phone,company,address,message) 
+        sendMailToCustomer(firstname,lastname,email)
+        return render(request, 'thankyou.html')
     return render(request, 'getintouch.html')
 
 def bicyclepumpparts(request):
@@ -43,3 +59,44 @@ def surgicalparts(request):
 
 def brassballvalve(request):
     return render(request, 'brass-ball-valve.html')
+
+def sendMailToHost(firstname,lastname,email,phone,comapny,address,message):
+    subject = "New Enquiry at Swastik Metal"
+    message = f'''
+    New Enquiry
+    
+    Name: {firstname} {lastname}
+    Email: {email}
+    Contact No: {phone}
+    Company Name: {comapny}
+    Address: {address}
+    Message: {message}
+    '''
+    email_from = f'Swastik Metal<{settings.EMAIL_HOST_USER}>'
+    recipient_list = ['iamyash226@gmail.com']
+    try:
+        send_mail(subject, message, email_from, recipient_list)
+    except Exception as e: 
+        print(f"Error sending email: {e}")
+
+
+def sendMailToCustomer(firstname,lastname,email):
+    subject = "Thank you for Enquiry at Swastik Metal"
+    message = f'''
+Hello, {firstname} {lastname}
+
+Thank you for reaching out to Swastik Metal. We appreciate your interest in our brass products and services. Your inquiry is important to us, and our team is already reviewing your request.
+
+We strive to respond to all inquiries promptly and will get back to you as soon as possible with the information you need. If you have any additional questions or require further assistance in the meantime, please feel free to contact us directly.
+
+Thank you once again for considering Swastik Metal. We look forward to the opportunity to assist you.
+
+Best regards,
+Yash Taraviya, COO
+Swastik Metal'''
+    email_from = f'Swastik Metal<{settings.EMAIL_HOST_USER}>'
+    recipient_list = [email]
+    try:
+        send_mail(subject, message, email_from, recipient_list)
+    except Exception as e: 
+        print(f"Error sending email: {e}")
